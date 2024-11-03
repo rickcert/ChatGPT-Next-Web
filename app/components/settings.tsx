1690f5ecd1e3724bde46e5b5f7c083ea700ee278
rick <rickhqh@foxmail.com>
2024-11-03 14:06:03 +0800

    fix :some bug

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -49,7 +49,7 @@ import Locale, {
   changeLang,
   getLang,
 } from "../locales";
-import { copyToClipboard, clientUpdate, semverCompare } from "../utils";
+import { copyToClipboard, clientUpdate } from "../utils";
 import Link from "next/link";
 import {
   Anthropic,
@@ -587,7 +587,7 @@ export function Settings() {
   const [checkingUpdate, setCheckingUpdate] = useState(false);
   const currentVersion = updateStore.formatVersion(updateStore.version);
   const remoteId = updateStore.formatVersion(updateStore.remoteVersion);
-  const hasNewVersion = semverCompare(currentVersion, remoteId) === -1;
+  const hasNewVersion = -1;
   const updateUrl = getClientConfig()?.isApp ? RELEASE_URL : UPDATE_URL;
 
   function checkUpdate(force = false) {
